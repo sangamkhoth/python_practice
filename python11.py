6381e0a7e49c7739f47166872f793ab24fce7851
sangamkhoth <sanjubishnoi076@gmail.com>
2022-11-30 20:01:04 +0530

    int function in python

---
+++ python11.py
@@ -0,0 +1,15 @@
+number_one = input("enter first number ")
+number_two = input("enter second number ")
+total = number_one + number_two
+print("total is "+ total)
+
+number_one = int(input("enter first number "))
+number_two = int(input("enter second number "))
+total = number_one + number_two
+print("total is "+ str(total))
+
+#str
+#4 -->"4"
+
+#int
+#"4" --> 4
