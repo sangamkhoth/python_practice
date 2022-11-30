634352494f840b0c9f36bfdb1bdaf450b91c3144
sangamkhoth <sanjubishnoi076@gmail.com>
2022-11-30 19:18:06 +0530

    comments in python

---
+++ python5.py
@@ -0,0 +1,8 @@
+# this is a comment
+print("line A\nline B \n line C")
+#\t is for tab
+print("name\tSangam")
+print("this is double backslash\\\\") # \\ is for backslash
+print("hell\blo")
+# comments
+# print("")
